ba1fe81d3085a792887a53d4f9230523f38eb152
WentaoLuo <wentao.luo@yale.edu>
2019-03-05 22:48:16 +0900

    tested good

--- gglens.py
+++ gglens.py
@@ -45,32 +45,31 @@ class ESD(object):
     s2[ix3] = 2.0/(x1[ix3]+1.0)*(np.log(0.5*x[ix3])+\
                x2[ix3]*np.arctan(x3[ix3]))
 
-    res = {'func':s2-s1}
+    res = {'gfunc':s2,'ffunc':s1}
     return res
 
   def NFWcen(self,Rp):
     functions = self.funcs(Rp)
-    funcf     = functions['func']
+    funcf     = functions['gfunc']-functions['ffunc']
     res       = self.amp*(funcf)
     return res
 
-  def NFWRoff(self,Rp,Rf):
+  def NFWRoff(self,Rp):
     nstp = 1000
-    #Rf   = np.linspace(0.01,0.9,nstp)
+    Rf   = np.linspace(0.01,0.9,nstp)
     the  = np.linspace(0.0,2.0*pi,nstp)
     sthe = 2.0*pi/nstp
     nxx  = len(Rp)
     tmp  = np.zeros((nstp,nxx))
     tmpsm= np.zeros((nstp,nxx))
     dsig = np.zeros((nstp,nxx))
-    for i in range(1):
+    for i in range(nstp):
       tmp1 = np.zeros((nstp,nxx)) 
       for j in range(nstp):
         cosa   = np.cos(the[j])
-        #xx     = np.sqrt(Rp*Rp+Rf[i]*Rf[i]+2.0*Rp*Rf[i]*cosa)
-        xx     = np.sqrt(Rp*Rp+Rf*Rf+2.0*Rp*Rf*cosa)
+        xx     = np.sqrt(Rp*Rp+Rf[i]*Rf[i]+2.0*Rp*Rf[i]*cosa)
 	funcf  = self.funcs(xx)
-        tmp1[j,:]= sthe*self.amp*funcf['func']
+        tmp1[j,:]= sthe*self.amp*(funcf['gfunc']-funcf['ffunc'])
       
       tmp[i,:] = 0.5*(tmp1[:,:]).sum(axis=0)/pi
       tmp2     = np.zeros((nstp,nxx))
@@ -81,7 +80,14 @@ class ESD(object):
         for ix in range(nstp):
 	   tmp2[ix,iy] = srp*interpesd[ix]*Rsub[ix]
         tmpsm[i,iy] = 2.0*(tmp2[:,iy].sum(axis=0))/Rp[iy]/Rp[iy]
-
       dsig[i,:]  = tmpsm[i,:]-tmp[i,:]
-    return dsig[0,:]
-#-----CLASS FINISHED-----------------------------------------------------------
+
+    return {'Roff':Rf,'subesd':dsig}
+
+  def nfw2ddens(self):
+    nn  = 100
+    Rpj = np.linspace(0.01,0.9,nn)
+    func= self.funcs(Rpj)
+    pdf = func['ffunc']/func['ffunc'].sum()
+    return {'Rpj':Rpj,'pdf':pdf}
+#-----CLASS FINISHED--------------------------------------------------
